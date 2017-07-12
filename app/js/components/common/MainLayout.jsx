5c0be7748d829d8774eb50bcab8aaee5e22c47df
JudeNiroshan <jude.niroshan11@gmail.com>
2017-07-12 18:33:49 +0530

    Merge commit

---
+++ app/js/components/common/MainLayout.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './MainLayout.css';
+import ReportNavComponent from './ReportNavComponent';
+import ListOfUsers from '../reports/ListOfUsers';
+
+
+
+/**
+ * This component will be shown in the left side which allows users to navigate through
+ * different available reports
+ */
+class MainLayout extends Component {
+
+
+    render(){
+        return (
+        <div className="pageContainer">
+            <div className="reportNavBar">
+                <ReportNavComponent />
+            </div>
+            <div className="reportContent">
+                <ListOfUsers />
+            </div>
+        </div>
+        );
+    }
+}
+
+export default MainLayout;
