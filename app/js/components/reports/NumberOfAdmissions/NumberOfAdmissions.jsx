809de4e015c9539a09c328fa205f2210ec62f901
Sumangala028 <sumangala028@gmail.com>
2018-06-18 18:39:59 +0530

    Added Number of Registration for all Locations to the Module
    Added bar chart to the Number of Admissions report

--- app/js/components/reports/NumberOfAdmissions/NumberOfAdmissions.jsx
+++ app/js/components/reports/NumberOfAdmissions/NumberOfAdmissions.jsx
@@ -3,6 +3,7 @@ import ReportAsTableView from '../common/ReportAsTableView';
 import ReportTitle from '../common/ReportTitle';
 import LocationInput from '../common/LocationInput';
 import InfoMessage from '../common/InfoMessage';
+import ReportAsPieChart from '../common/ReportAsPieChart';
 
 /**
  * Display the result of Number of Admissions report
@@ -17,11 +18,16 @@ class NumberOfAdmissions extends Component {
       }
     };
     this.getReportUUID = this.getReportUUID.bind(this);
+    this.getReportUUIDForAllLocationsReport = this.getReportUUIDForAllLocationsReport.bind(this);
     this.handleLocationSelector = this.handleLocationSelector.bind(this);
   }
 
   getReportUUID() {
     return "d39509bc-4881-11e7-a919-92ebcb67fe33";
+  }
+
+  getReportUUIDForAllLocationsReport() {
+    return "8fd12cfb-b0d5-4991-8acf-b90aefe7f86c";
   }
 
   handleLocationSelector(event) {
@@ -37,14 +43,21 @@ class NumberOfAdmissions extends Component {
       <div>
         <ReportTitle heading="Number of Admissions" />
         <LocationInput locationListener={this.handleLocationSelector} />
-
+        
         {this.state.parameters.location != null && this.state.parameters.location != '' ? (
 
           <ReportAsTableView reportUUID={this.getReportUUID()}
             reportParameters={this.state.parameters} />
 
         ) : (
-          <InfoMessage componentName="location" />
+          <div>
+          <ReportAsTableView reportUUID={this.getReportUUIDForAllLocationsReport()}
+            reportParameters={this.state.parameters} />
+            
+            <ReportAsPieChart reportUUID={this.getReportUUIDForAllLocationsReport()}
+            reportParameters={this.state.parameters}
+            labels="Location Name" qty="Count" limit={10} />
+          </div>
         )}
       </div>
     );
