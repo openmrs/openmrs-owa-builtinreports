991df59fe7a0caf36eade533b0735a271fe9a4e5
Sumangala028 <sumangala028@gmail.com>
2018-06-28 11:57:31 +0530

    BIR-7 Added Number of Discharges for all Locations Reports to the Module

--- app/js/components/reports/NumberOfDischarges/NumberOfDischarges.jsx
+++ app/js/components/reports/NumberOfDischarges/NumberOfDischarges.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReportAsTableView from '../common/ReportAsTableView';
 import ReportTitle from '../common/ReportTitle';
 import LocationInput from '../common/LocationInput';
-import InfoMessage from '../common/InfoMessage';
+import ReportAsPieChart from '../common/ReportAsPieChart';
 
 /**
  * Display the result of Number of Discharges report
@@ -17,11 +17,16 @@ class NumberOfDischarges extends Component {
       }
     };
     this.getReportUUID = this.getReportUUID.bind(this);
+    this.getReportUUIDForAllLocationsReport = this.getReportUUIDForAllLocationsReport.bind(this);
     this.handleLocationSelector = this.handleLocationSelector.bind(this);
   }
 
   getReportUUID() {
     return "b39c5070-4881-11e7-a919-92ebcb67fe33";
+  }
+
+  getReportUUIDForAllLocationsReport() {
+    return "d1a00e10-cecb-4775-8c4f-2ed0b059d7b9";
   }
 
   handleLocationSelector(event) {
@@ -44,7 +49,14 @@ class NumberOfDischarges extends Component {
             reportParameters={this.state.parameters} />
 
         ) : (
-          <InfoMessage componentName="location" />
+          <div>
+          <ReportAsTableView reportUUID={this.getReportUUIDForAllLocationsReport()}
+            reportParameters={this.state.parameters} />
+            
+            <ReportAsPieChart reportUUID={this.getReportUUIDForAllLocationsReport()}
+            reportParameters={this.state.parameters}
+            labels="Location Name" qty="Count" limit={10} />
+          </div>
         )}
       </div>
     );
